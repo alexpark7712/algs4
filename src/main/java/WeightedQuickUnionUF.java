1368bd87a2648c354be1e6f0e809868ba9e267df
Ubuntu <ubuntu@ip-172-31-52-184.us-west-2.compute.internal>
2023-03-15 23:56:55 +0000

    weighted quick union find

---
+++ src/main/java/WeightedQuickUnionUF.java
@@ -0,0 +1,65 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class WeightedQuickUnionUF {
+    private int[] parent;
+    private int[] size;
+    private int count;
+
+    public WeightedQuickUnionUF(int n) {
+        count = n;
+        parent = new int[n];
+        size = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    public int count() {
+        return count;
+    }
+
+    public int find(int p) {
+        while(parent[p] != p) {
+            p = parent[p];
+        }
+        return p;
+    }
+
+    public boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+
+        if(rootP == rootQ) {
+            return;
+        }
+
+        if(size[rootP] < size[rootQ]) {
+            parent[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        } else {
+            parent[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+        }
+        count --;
+    }
+
+    public static void main(String[] args) {
+        int n = StdIn.readInt();
+        WeightedQuickUnionUF uf = new WeightedQuickUnionUF(n);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (uf.connected(p, q)) continue;
+            uf.union(p, q);
+            StdOut.println(p + " " + q);
+        }
+        StdOut.println(uf.count() + " components");
+        StdOut.println(uf.connected(4, 8));
+    }
+}
