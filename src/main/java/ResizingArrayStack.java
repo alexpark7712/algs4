5e1e7b6a9ebd05d639f2ae81495bae0f7315a682
Ubuntu <ubuntu@ip-172-31-52-184.us-west-2.compute.internal>
2023-03-30 04:11:24 +0000

    stack

---
+++ src/main/java/ResizingArrayStack.java
@@ -0,0 +1,68 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.NoSuchElementException;
+
+public class ResizingArrayStack<Item> {
+    private static final int INIT_CPACITY = 4;
+
+    private Item[] a;
+    private int n;
+
+    public ResizingArrayStack() {
+        a = (Item[]) new Object[INIT_CPACITY];
+        n = 0;
+    }
+
+    public boolean isEmpty() {
+        return n == 0;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public void push(Item item) {
+        if (n == a.length) {
+            resize(2 * a.length);
+        }
+
+        a[n++] = item;
+    }
+
+    public Item pop() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Stack underflow");
+        }
+        Item item = a[n - 1];
+        a[n - 1] = null;
+        n--;
+
+        if (n > 0 && n == a.length / 4) {
+            resize(a.length / 2);
+        }
+
+        return item;
+    }
+
+    private void resize(int capacity) {
+        Item[] copy = (Item[]) new Object[capacity];
+        for (int i = 0; i < n; i++) {
+            copy[i] = a[i];
+        }
+        a = copy;
+    }
+
+    public static void main(String[] args) {
+        ResizingArrayStack<String> stack = new ResizingArrayStack<>();
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-")) {
+                stack.push(item);
+            } else if (!stack.isEmpty()) {
+                StdOut.print(stack.pop() + " ");
+            }
+        }
+        StdOut.println("(" + stack.size() + " left on stack)");
+    }
+}
---
+++ src/main/java/Stack.java
@@ -0,0 +1,58 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.NoSuchElementException;
+
+public class Stack<Item> {
+    private static class Node<Item> {
+        private Item item;
+        private Node<Item> next;
+    }
+
+    private Node<Item> first;
+    private int n;
+
+    public Stack() {
+        first = null;
+        n = 0;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public void push(Item item) {
+        Node<Item> oldFirst = first;
+        first = new Node<>();
+        first.item = item;
+        first.next = oldFirst;
+        n++;
+    }
+
+    public Item pop() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Stack underflow");
+        }
+        Item item = first.item;
+        first = first.next;
+        n--;
+        return item;
+    }
+
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-")) {
+                stack.push(item);
+            } else if (!stack.isEmpty()) {
+                StdOut.print(stack.pop() + " ");
+            }
+        }
+        StdOut.println("(" + stack.size() + " left on stack)");
+    }
+}
